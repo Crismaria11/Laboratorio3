c288dab5a8c798d38b835756570d89b7eac33097
Crismaria11 <38018701+Crismaria11@users.noreply.github.com>
2018-07-19 12:14:51 -0600

    Add files via upload

---
+++ Laboratorio3.kt
@@ -0,0 +1,31 @@
+package main
+
+
+fun menu0(): String {
+    return """
+        Menu:
+        1. Crear lista
+        2. Salir
+        """.trimIndent()
+}
+
+fun MenuCL0(): String {
+    return """
+        Menu:
+        1. Crear lista
+        2. Ver listas
+        3. Seleccionar lista
+        4. Salir
+        """.trimIndent()
+}
+
+fun menuCLS(): String {
+    return """
+        Menu:
+        1. Agregar lista
+        2. Agegar tarea
+        3. Ver listas
+        4. Ver tareas de listas
+        5. Salir
+        """.trimIndent()
+}
---
+++ Listas2.kt
@@ -0,0 +1,5 @@
+package listas
+
+fun main(args: Array<Boolean>) {
+    
+}
